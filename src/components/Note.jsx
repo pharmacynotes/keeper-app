d10ac848af984784cfd2d41de0d5afd68fa5a9b6
pharmacynotes <himrajdeeraj+github1@gmail.com>
2023-07-22 22:53:59 +0530

    first version

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>
+            This is the title
+        </h1>
+    </div>
+}
+
+export default Note;
