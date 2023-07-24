46924675090a36f340985d1d59dc922b54db8a12
pharmacynotes <95494703+pharmacynotes@users.noreply.github.com>
2023-07-25 00:23:19 +0530

    h

--- src/index.js
+++ src/index.js
@@ -2,10 +2,38 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import App from "./components/App";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img 
+        src={props.img}
+        alt="avatar_img"
+
+      />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <App />,
+  <div>
+    <h1>My contacts</h1>
+    <Card name="Beyonce" img="https://th.bing.com/th/id/OIP.OJI1pdGG-pXP2N-_xaEXXwHaE8?w=253&h=180&c=7&r=0&o=5&pid=1.7" tel="+3083452085028" email="oweuljaf@gmail.com"/>
+    <br></br>
+    <Card name="Beyonce" img="https://th.bing.com/th/id/OIP.mrOGiAOJfi7FR0ud4FfxewHaEo?w=282&h=180&c=7&r=0&o=5&pid=1.7" tel="+324440258" email="oafdf@gmail.com"/>
+  </div>,
   document.getElementById("root")
+
 );
+
+
+
+
+
+
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
